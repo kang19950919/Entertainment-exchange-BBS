5af4dff8213277a3c8cdf2137f0cebbe1ee5f8c8
yuankang <17600823404@163.com>
2019-06-07 00:48:04 +0800

    集成配置类和SQLAlchemy

--- manage.py
+++ manage.py
@@ -1,6 +1,24 @@
+"""
+一、集成配置类
+二、集成sqlalchemy到flask
+"""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    DUBUG = True
+
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/bbs"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 
 app = Flask(__name__)
+# 一、集成配置类
+app.config.from_object(Config)
+
+# 二、集成sqlalchemy到flask
+db = SQLAlchemy(app)
 
 
 @app.route("/")
