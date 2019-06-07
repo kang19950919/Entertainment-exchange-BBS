374933cd3718bbd9f939c510dbf6241c926631de
yuankang <17600823404@163.com>
2019-06-07 16:16:02 +0800

    抽取app业务逻辑到info目录下

--- manage.py
+++ manage.py
@@ -7,32 +7,10 @@
 六、集成flask-script
 七、集成flask-migrate
 """
-from flask_migrate import MigrateCommand, Migrate
-from flask import Flask, session
-from flask_sqlalchemy import SQLAlchemy
-from redis import StrictRedis
-from flask_wtf import CSRFProtect
-from flask_session import Session
+
 from flask_script import Manager
-from config import Config
-
-
-app = Flask(__name__)
-# 一、集成配置类
-app.config.from_object(Config)
-
-# 二、集成sqlalchemy到flask
-db = SQLAlchemy(app)
-
-# 三、集成redis  可以吧容易产生变化的值放入到配置中
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
-# 四、CSRFProtect, 只起到保护的作用，具体往表单和cookie中设置csrf_token还需要我们自己去做
-CSRFProtect(app)
-
-# 集成flask-session
-# 说明: flask中的session是保存用户数据的容器（上下文），而flask_session中的Session是指定session的保存路径
-Session(app)
+from flask_migrate import MigrateCommand, Migrate
+from info import app, db
 
 # 六、集成flask-script
 manager = Manager(app)
