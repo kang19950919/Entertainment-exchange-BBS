078962143c7653b0d936240c34bafc28563d2409
yuankang <17600823404@163.com>
2019-06-08 13:15:02 +0800

    解决favicon问题

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,14 @@
 from info.modules.index import index_blu
-from flask import render_template
+from flask import render_template, send_file, redirect, current_app
 
 
 @index_blu.route("/")
 def index():
     return render_template("news/index.html")
+
+
+@index_blu.route("/favicon.ico")
+def favicon():
+    # return send_file("E:/git_flask/BBS/Entertainment-exchange-BBS/info/static/news/favicon.ico")
+    # return redirect("/static/news/favicon.ico")
+    return current_app.send_static_file("news/favicon.ico")
