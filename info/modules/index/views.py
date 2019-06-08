79c0c9e2fa0874926daf96e4f8bb813d11f70add
yuankang <17600823404@163.com>
2019-06-08 13:24:00 +0800

    添加注释

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -9,6 +9,8 @@ def index():
 
 @index_blu.route("/favicon.ico")
 def favicon():
+    # 绝对路径，不太好
     # return send_file("E:/git_flask/BBS/Entertainment-exchange-BBS/info/static/news/favicon.ico")
+    # 重定向，不太好
     # return redirect("/static/news/favicon.ico")
     return current_app.send_static_file("news/favicon.ico")
