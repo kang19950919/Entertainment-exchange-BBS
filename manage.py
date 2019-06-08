c1fbce57b808ad9fe58a3f81da0227af7cf99553
yuankang <17600823404@163.com>
2019-06-08 10:59:36 +0800

    数据库迁移

--- manage.py
+++ manage.py
@@ -11,6 +11,7 @@
 from flask_script import Manager
 from flask_migrate import MigrateCommand, Migrate
 from info import create_app, db
+from info.models import *
 
 # 通过传入不同配置创造出不同配置下的app实例， 工厂方法  python设计模式：工厂模式
 app = create_app("develop")
