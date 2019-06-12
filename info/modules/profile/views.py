e92f10302f06eaa8560bc426365c3fad9166b475
yuankang <17600823404@163.com>
2019-06-12 17:10:51 +0800

    个人中心渲染

---
+++ info/modules/profile/views.py
@@ -0,0 +1,19 @@
+from flask import g, redirect, render_template
+
+from info.utils.common import user_login
+from . import profile_blu
+
+
+@profile_blu.route("/info")
+@user_login
+def user_info():
+
+    user = g.user
+    if user:
+        return redirect("/")
+
+    data = {
+        "user": user.to_dict()
+    }
+
+    return render_template("news/user.html")
--- info/utils/common.py
+++ info/utils/common.py
@@ -1,3 +1,8 @@
+import functools
+
+from flask import session, current_app
+
+
 def do_index_class(index):
     if index == 1:
         return "first"
@@ -6,3 +11,19 @@ def do_index_class(index):
     if index == 3:
         return "third"
     return ""
+
+
+def user_login(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        user_id = session.get("user_id")
+        user = None
+        if user_id:
+            try:
+                from info.models import User
+                user = User.query.get(user_id)
+            except Exception as e:
+                current_app.logger.error(e)
+        g.user = user
+        return func(*args, **kwargs)
+    return wrapper
