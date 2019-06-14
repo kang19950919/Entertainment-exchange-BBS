c8d7d65ce266740cf7691dd7c940cf6ec598ee5c
chengyuankang <17600823404@163.com>
2019-06-14 08:41:36 +0800

    新闻评论的后端逻辑

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,10 +1,65 @@
 from flask import render_template, g, current_app, abort, jsonify, request
 
-from info import constants
-from info.models import News
+from info import constants, db
+from info.models import News, Comment
 from info.utils.common import user_login
 from info.utils.response_code import RET
 from . import news_blu
+
+
+@news_blu.route("/news_comment", methods=['POST'])
+def news_comment():
+    """
+    新闻评论功能
+    1、接收参数 用户 新闻 评论内容 parent_id
+    2、校验参数
+    3、业务逻辑， 往数据库中添加一条评论
+    4、返回响应
+    :return:
+    """
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    news_id = request.json.get("news_id")
+    comment_str = request.json.get("comment_id")
+    parent_id = request.json.get("parent_id")
+
+    if not all([news_id, comment_id]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不正确")
+    try:
+        news_id = int(news_id)
+        if parent_id:
+            parent_id = int(parent_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="数据格式不正确")
+
+    try:
+        news = News.query.get(News.id == news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="该条新闻不存在")
+
+    # 添加新闻评论，王评论表中添加一条数据
+    comment = Comment()
+    comment.user_id = user.id
+    comment.news_id = news_id
+    comment.content = comment_str
+    if parent_id:
+        comment.parent_id = parent_id
+
+    try:
+        db.session.add(comment)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库保存失败")
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @news_blu.route("/news_collect", methods=["POST"])
